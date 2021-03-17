486cc07af842528a9d7140a430cb4a2faaf54bf6
Colin Stoetzner <colin.r.stoetzner@accenture.com>
2021-03-17 11:23:20 -0400

    first commit

---
+++ src/main/kotlin/com/zoo/virtual/virtualzoo/VirtualZooApplication.kt
@@ -0,0 +1,11 @@
+package com.zoo.virtual.virtualzoo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VirtualZooApplication
+
+fun main(args: Array<String>) {
+	runApplication<VirtualZooApplication>(*args)
+}
---
+++ src/test/kotlin/com/zoo/virtual/virtualzoo/VirtualZooApplicationTests.kt
@@ -0,0 +1,14 @@
+package com.zoo.virtual.virtualzoo
+
+
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VirtualZooApplicationTests {
+
+	@Test
+	fun contextLoads() {
+		fail("failing on purpose")
+	}
+
+}
